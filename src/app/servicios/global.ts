53e30b4d6903f1816a4b580a90c16c8c1b0477ed
eljezer <jezerrazuri@upeu.edu.pe>
2022-08-10 18:30:46 -0500

    AS

--- src/app/servicios/global.ts
+++ src/app/servicios/global.ts
@@ -1,3 +1,2 @@
-// export const apiURL: string = 'https://api.freenetxa.ml/api';
-export const apiURL: string = 'http://localhost:3000/api';
-
+export const apiURL: string = 'https://api.freenetxa.ml/api';
+// export const apiURL: string = 'http://localhost:3000/api';
